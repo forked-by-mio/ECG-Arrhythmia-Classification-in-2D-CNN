a7121817377b671bfc078bdc5f3110688fbb2c16
澪 <mio@mio19.uk>
2023-11-28 11:06:10 +0800

    tf_upgrade_v2

--- test_model.py
+++ test_model.py
@@ -12,9 +12,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.45
-set_session(tf.Session(config=config))
+set_session(tf.compat.v1.Session(config=config))
 
 def main():
     # init model
--- train_model_personal.py
+++ train_model_personal.py
@@ -20,9 +20,9 @@ import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.45
-set_session(tf.Session(config=config))
+set_session(tf.compat.v1.Session(config=config))
 
 
 def plot_history(history, result_dir):
